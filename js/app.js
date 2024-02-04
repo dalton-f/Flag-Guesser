18f091f781cc7fb3f6b507bb56fd88a85670de04
Dalton <daltonfiggdavies@gmail.com>
2024-02-04 08:23:43 +0000

    Update API call

--- js/app.js
+++ js/app.js
@@ -1,17 +1,10 @@
-// Create a new XMLHttpRequest object
-const request = new XMLHttpRequest();
-
-// Initialize the request with a "GET" request to the REST Countries API
-request.open(
-  "GET",
+fetch(
   "https://restcountries.com/v3.1/independent?status=true&fields=name,flags"
-);
-
-// Send the initialized request
-request.send();
-
-// Set up an event handler for the "load" event, which is triggered when the request is successfully completed
-request.onload = () => {
-  // Parse the JSON response received from the server and store it in the 'result' variable
-  const result = JSON.parse(request.response);
-};
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
