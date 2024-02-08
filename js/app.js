f171a4d5b3f6ece82c2f53f0873da7fa0262515b
Dalton <daltonfiggdavies@gmail.com>
2024-02-08 17:42:30 +0000

    Improve API call to handle errors

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
+// URL prefiltered to only return independent countries, and only receiving the name + flags to ensure the call is more efficient
+
 const url =
   "https://restcountries.com/v3.1/independent?status=true&fields=name,flags";
 
 const retrieveData = async () => {
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    throw new Error(error);
+    // Handles any 400s or 500s status codes
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (err) {
+    // Handles network or CORS issues
+    throw new Error(err);
   }
 };
 
